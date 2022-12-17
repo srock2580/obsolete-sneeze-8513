a02624078c90b8d5e041507eb5c14eed71177983
srock2580 <srock2580@gmail.com>
2022-12-17 23:13:48 +0530

    update

--- sugarcosmetics/src/Pages/Makeup.jsx
+++ sugarcosmetics/src/Pages/Makeup.jsx
@@ -1,5 +1,8 @@
+import { Item } from '../Components/Item'
+import makeupData from '../makeupData.json'
+
 export const Makeup = () => {
-  return <div>
-    Makeup
+  return <div className='makeup'>
+    
   </div>
 }
